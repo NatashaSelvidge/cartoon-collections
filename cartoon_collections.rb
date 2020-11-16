20fe9c69065658a8d0150dc30e2ce1f5a0f42241
Natasha Selvidge <Natasha.selvidge@gmail.com>
2020-11-16 11:02:00 -0800

    commit tree c75f703543da13c1297708af10d3c8db8eee5375

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,23 +4,28 @@ def roll_call_dwarves(dwarves)# code an argument here
   end 
 end
 
+
+
+
 def summon_captain_planet(veggies)# code an argument here
    veggies.map! do |veg|
     "#{veg.capitalize}!"
    end 
 end
 
-def long_planeteer_calls(calls_long)# code an argument here
+def long_planeteer_calls(calls_long)
   calls_long.any? do |calls|
     calls.length > 4
   end 
 end
 
 
-def find_the_cheese(cheese)# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese) 
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese.find do |cheese|
     cheese_types.include?(cheese)
   end
 end
+
+
+
